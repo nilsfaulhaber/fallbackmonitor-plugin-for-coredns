6606f3230e87b6cfba451d99f6a5eacbd00dabe3
Nils Faulhaber <faulhabn@vmott50.in.tum.de>
2022-04-21 13:17:14 +0200

    add plugin

---
+++ fallbackmonitor.go
@@ -0,0 +1,129 @@
+package fallbackmonitor
+
+import (
+	"fmt"
+	"context"
+	"strings"
+	"encoding/csv"
+	"log"
+	"os"
+	"time"
+"strconv"
+	"github.com/coredns/coredns/plugin"
+	"github.com/coredns/coredns/plugin/metadata"
+	"github.com/coredns/coredns/request"
+	"util"
+	"github.com/miekg/dns"
+)
+
+// Handler is a plugin handler that takes a query and templates a response.
+type Handler struct {
+	Zones []string
+	Next      plugin.Handler
+}
+
+
+type queryData struct {
+	Name     string
+	Remote	 string
+	Message  *dns.Msg
+	md       map[string]metadata.Func
+}
+
+func (data *queryData) Meta(metaName string) string {
+	if data.md == nil {
+		return ""
+	}
+
+	if f, ok := data.md[metaName]; ok {
+		return f()
+	}
+
+	return ""
+}
+
+// ServeDNS implements the plugin.Handler interface.
+func (h Handler) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
+	state := request.Request{W: w, Req: r}
+
+	protocol, _ := util.GetProtocolFromContext(ctx)
+        fmt.Println("DNS Request over", protocol)
+
+	data := getData(ctx, state)
+	msg := new(dns.Msg)
+	msg.SetReply(r)
+	msg.Authoritative = true
+	msg.Rcode = dns.RcodeSuccess
+
+	rrs, err := assembleRR(data, protocol)
+	if err != nil {
+	   return dns.RcodeServerFailure, err
+	}
+
+	for i := range rrs {
+	  msg.Answer = append(msg.Answer, rrs[i])
+	}
+
+	w.WriteMsg(msg)
+	return dns.RcodeSuccess, nil
+
+}
+
+// Name implements the plugin.Handler interface.
+func (h Handler) Name() string { return "fallbackmonitor" }
+
+func assembleRR(data *queryData, protocol string) ([]dns.RR, error) {
+	from_str := fmt.Sprintf("FROM_%s Protocol_%s", data.Remote, protocol)
+
+	buffer_str := strings.Replace(data.Message.String(), "\n", "$", -1)
+	buffer_str = strings.Replace(buffer_str, " ", "&", -1)
+	buffer_str = strings.Replace(buffer_str, ";;", " ", -1)
+	buffer_str = strings.Replace(buffer_str, ";", "%", -1)
+	buffer_str = strings.Replace(buffer_str, "\t", "?", -1)
+	buffer_str = strings.Replace(buffer_str, " &", " ", -1)
+	buffer_str = strings.Replace(buffer_str, "& ", " ", -1)
+
+
+	csvFile, err := os.OpenFile("/home/faulhabn/request_data.csv", os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+
+	if err != nil {
+		log.Fatalf("failed to open file file: %s", err)
+	}
+
+	timestamp := strconv.FormatInt(time.Now().Unix(), 10)
+	csvwriter := csv.NewWriter(csvFile)
+	csvwriter.Comma = ';'
+	content := fmt.Sprintf("%s %s", from_str, buffer_str)
+	row := []string{data.Name, timestamp, content}
+	erro := csvwriter.Write(row)
+	if erro != nil {
+	   fmt.Println(erro)
+	   log.Fatalf("failed to write line: %s", erro)
+	}
+
+	csvwriter.Flush()
+	if err := csvwriter.Error(); err != nil {
+	  log.Fatal(err)
+	}
+
+	csvFile.Close()
+
+	rrs := make([]dns.RR, 145)
+
+	for a := 0; a < 145; a++ {
+	  base_str := fmt.Sprintf("%s IN AAAA 2003:ec:970e:f439:c5fd:30b8:2365:%x", data.Name, a)
+	  rr, err := dns.NewRR(base_str)
+          if err != nil {
+             return rrs, err
+          }
+	 rrs[a] = rr
+	}
+	return rrs, nil
+}
+
+func getData(ctx context.Context, state request.Request) (*queryData) {
+	data := &queryData{md: metadata.ValueFuncs(ctx), Remote: state.IP()}
+	data.Name = state.Name()
+        data.Message = state.Req
+	return data
+}
---
+++ setup.go
@@ -0,0 +1,28 @@
+package fallbackmonitor
+
+import (
+	"github.com/coredns/caddy"
+	"github.com/coredns/coredns/core/dnsserver"
+	"github.com/coredns/coredns/plugin"
+)
+
+func init() { plugin.Register("fallbackmonitor", setupEcho) }
+
+func setupEcho(c *caddy.Controller) error {
+	handler, err := echoParse(c)
+	if err != nil {
+		return plugin.Error("fallbackmonitor", err)
+	}
+	dnsserver.GetConfig(c).AddPlugin(func(next plugin.Handler) plugin.Handler {
+		handler.Next = next
+		return handler
+	})
+
+	return nil
+}
+
+func echoParse(c *caddy.Controller) (handler Handler, err error) {
+	zones := plugin.OriginsFromArgsOrServerBlock(c.RemainingArgs(), c.ServerBlockKeys)
+	handler.Zones = append(handler.Zones, zones...)
+	return
+}
